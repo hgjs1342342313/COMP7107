c8e965e79af11e977ffea7122d83e8ec8ea97391
Zheming Kang <zkang018@connect.hku.hk>
2024-03-02 03:14:10 +0800

    Submit of Assignment 2

---
+++ Assignment1/src/foo.py
@@ -0,0 +1,16 @@
+import numpy as np 
+ 
+x = np.array([3,  1,  2])  
+print ('我们的数组是：')
+print (x)
+print ('\n')
+print ('对 x 调用 argsort() 函数：')
+y = np.argsort(x)  
+print (y)
+print ('\n')
+print ('以排序后的顺序重构原数组：')
+print (x[y])
+print ('\n')
+print ('使用循环重构原数组：')
+for i in y:  
+    print (x[i], end=" ")
